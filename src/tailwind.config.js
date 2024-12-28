e1a08d9cebcecca62fa75e75e48c4a0b20a44a6c
Praveen <praveen270304@gmail.com>
2024-12-28 20:31:45 +0530

    Commit Changes

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -4,18 +4,17 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'mobile': '300px',
+      'mobile': '300px',
 
-        'tablet': '640px',
-        // => @media (min-width: 640px) { ... }
-  
-        'laptop': '800px',
-        // => @media (min-width: 1024px) { ... }
-  
-        'desktop': '1280px',
-        // => @media (min-width: 1280px) { ... }
-      },
-    },
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '800px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },},
   },
   plugins: [],
 }
